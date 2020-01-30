52b78925ea4e8fbe0fb7da9b16404a247fd561a4
Alex MacArthur <alex.macarthur@daveramsey.com>
2020-01-30 14:48:28 -0600

    Move things around.

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,17 +51,6 @@ module.exports = {
     `gatsby-plugin-postcss`,
     `gatsby-plugin-preact`,
     {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        whitelist: ['StripeElement'], // Don't remove this selector
-        ignore: ["prismjs/"] // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      }
-    },
-    {
       resolve: `gatsby-plugin-sass`,
       options: {
         postCssPlugins: [require("tailwindcss")]
@@ -77,6 +66,17 @@ module.exports = {
           },
         ],
       },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        whitelist: ['StripeElement'], // Don't remove this selector
+        ignore: ["prismjs/", "/src/scss/root.scss"] // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
     },
     {
       resolve: "gatsby-plugin-react-svg",
