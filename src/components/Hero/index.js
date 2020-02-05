56afd5e7f738fc97c9e3cff18c745caa05162051
Alex MacArthur <alex.macarthur@daveramsey.com>
2020-02-05 07:11:56 -0600

    Use typeit-react for hero.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,48 +1,33 @@
-import React, { useEffect } from "react";
-import TypeIt from "typeit";
+import React from "react";
+import TypeIt from "typeit-react";
 
-export default function(props) {
-  let element = null;
-
-  useEffect(() => {
-    if (element === null) {
-      return;
-    }
-
-    let instance = new TypeIt(element, {
-      speed: 50,
-      startDelay: 900
-    })
-      .type("The most versatile animated typing utility on the internte")
-      .pause(500)
-      .options({ speed: 75, deleteSpeed: 300 })
-      .delete(2)
-      .pause(250)
-      .type("et")
-      .pause(750)
-      .options({ speed: 100, deleteSpeed: 75 })
-      .delete(8)
-      .pause(750)
-      .type("<em>planet.</em>")
-      .go();
-
-      return () => {  
-        instance.destroy(true);
-      }
-      
-      // eslint-disable-next-line
-  }, []);
-
+export default function({snippet}) {
   return (
     <div className="relative max-w-3xl mx-auto pt-0 md:pt-20 px-3 z-10">
-      <h1
-        aria-label="The most versatile animated typing utility on the planet."
+      <TypeIt 
+        element={"h1"}
         className="text-center mb-20 text-4xl md:text-5xl"
-        ref={el => {
-          element = el;
+        aria-label="The most versatile animated typing utility on the planet."
+        options={{
+          speed: 50,
+          startDelay: 900
         }}
-      ></h1>
-      <div dangerouslySetInnerHTML={{ __html: props.snippet }} />
+        getBeforeInit={(instance) => {
+          return instance
+            .type("The most versatile animated typing utility on the internte")
+            .pause(500)
+            .options({speed: 75, deleteSpeed: 300 })
+            .delete(2)
+            .pause(250)
+            .type("et")
+            .pause(750)
+            .options({speed: 100, deleteSpeed: 75 })
+            .delete(8)
+            .pause(750)
+            .type("<em>planet.</em>");
+        }}
+      />
+      <div dangerouslySetInnerHTML={{ __html: snippet }} />
     </div>
   );
 }
