9060caeb26dabbd051c3c853c80c7d3d8e6d568d
Alex MacArthur <alex@macarthur.me>
2022-07-17 14:28:48 -0500

    Change pricing.

--- licenseOptions.js
+++ licenseOptions.js
@@ -5,7 +5,7 @@ module.exports = [
     name: "TypeIt - Limited License",
     htmlTitle: "<strong>Limited</strong> Commercial License",
     simpleTitle: "Limited License",
-    price: 1900, // must be in cents!
+    price: 900, // must be in cents!
     description:
       "Use TypeIt on a single commercial project. Includes lifetime updates.",
     permissionDescription:
@@ -20,7 +20,7 @@ module.exports = [
     name: "TypeIt - Unlimited License",
     htmlTitle: "<strong>Unlimited</strong> Commercial License",
     simpleTitle: "Unlimited License",
-    price: 4400, // must be in cents!
+    price: 2900, // must be in cents!
     description:
       "Unlimited use on any commercial project. Includes lifetime updates.",
     permissionDescription:
